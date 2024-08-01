fd2badb36d282c94ee2a2942ab03953a782bb9da
shrikant2201 <shrikant2201@gmail.com>
2024-08-01 14:35:09 +0530

    flake8 changes

--- .github/workflows/test_model.py
+++ .github/workflows/test_model.py
@@ -1,8 +1,7 @@
 # test_model.py
-import pytest
-
 from model import model, X_test, y_test
 from sklearn.metrics import mean_squared_error
+
 
 def test_model_performance():
     predictions = model.predict(X_test)
--- model.py
+++ model.py
@@ -9,7 +9,10 @@ X = np.random.rand(100, 1)
 y = 3 * X.squeeze() + 2 + np.random.randn(100) * 0.5
 
 # Split the data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                                    y,
+                                                    test_size=0.2,
+                                                    random_state=42)
 
 # Train a simple linear regression model
 model = LinearRegression()
